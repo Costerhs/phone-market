f5ac1c6bfe3c5f326d0a394d25c3763631e243cb
Azybekov Nuralim <nur.cost.lpoi@gmail.com>
2022-06-03 23:14:40 +0600

    webpack,refactoring functional and css,dev server,readme

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,6 @@
+import json from './../config.json';
+import '../css/style.css';
+
 import openCloseForm from './modules/form.js';
 import timer from './modules/timer.js'
 import toggleClass from './modules/toggleMode.js';
@@ -5,9 +8,6 @@ import sendData from './modules/sendData.js';
 import setLink from './modules/setLink.js';
 import showBurger from './modules/showBurger.js';
 import scrollToMain from './modules/scrollToMain.js';
-
-import json from './../config.json';
-import '../css/style.css';
 import testimonialsCreateItem from './modules/testimonialsCreateItem.js';
 import pricingCreateItem from './modules/pricingCreateItem.js';
 
